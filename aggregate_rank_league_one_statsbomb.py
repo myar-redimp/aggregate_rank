cdf71e25316966d4f9d330c38a2adc856eb3740d
myar-redimp <myar@theredimps.com>
2025-05-01 15:57:53 +0100

    changed missing value handling
    average rank assigned for missing metrics

--- aggregate_rank_league_one_statsbomb.py
+++ aggregate_rank_league_one_statsbomb.py
@@ -13,8 +13,19 @@ def calculate_percentiles_league_one(df, comparable_positions, general_metrics):
 
     # Convert each metric to percentile in filtered_df using ther rankdata function
     for metric in general_metrics:
-      general_df[metric] = general_df[metric].fillna(0) # Fill missing values with nan
-      general_df[f'{metric}_percentile'] = rankdata(general_df[metric], method='average') / len(general_df) * 100
+
+        # Create a mask for NaN values
+        nan_mask = general_df[metric].isna()
+        
+        # Filter out NaN values for ranking
+        non_nan_values = general_df.loc[~nan_mask, metric]
+        
+        # Rank the non-NaN values and convert to percentage
+        ranked_percentages = rankdata(non_nan_values, method='average') / len(non_nan_values) * 100
+
+        #assign rank to new column, with 50 given to those who didnt record anything 
+        general_df[f'{metric}_percentile'] = 50.0
+        general_df.loc[~nan_mask, f'{metric}_percentile'] = ranked_percentages.astype(np.float64)
 
     # Create average rank column (mean across percentile columns)
     general_df['average_rank'] = general_df[[f'{metric}_percentile' for metric in general_metrics]].mean(axis=1)
